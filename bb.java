8af4c35ad4fe8e717abed77a099d93821f43d810
punithavathis <punithavathisakthivel97@gmail.com>
2017-07-17 14:37:47 +0530

    Add files via upload

---
+++ bb.java
@@ -0,0 +1,21 @@
+package guviprogram;
+
+public class bb {
+	public static void main(String[] args){
+		
+		
+		int i,j,k;
+		for(i=4;i<80;i++) 
+		{
+		         k=0;
+		         for(j=2;j<i;j++)
+		         {
+		                  if(i%j==0)
+		                  k++;
+		         }
+		         if(k==0)
+		         System.out.print(i+"   ");
+		}
+		}
+	
+}
