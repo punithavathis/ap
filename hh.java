46706db4fc5530f379a5a49d698933c3d389ab4b
punithavathis <punithavathisakthivel97@gmail.com>
2017-07-19 11:55:42 +0530

    Add files via upload

---
+++ hh.java
@@ -0,0 +1,11 @@
+package guviprogram;
+
+public class hh {
+	public static void main(String[] args){
+	
+	 String string = "abcdef";
+     String reverse = new StringBuffer(string).reverse().toString();
+     System.out.println("\nString before reverse: "+string);
+     System.out.println("String after reverse: "+reverse);
+}
+}
