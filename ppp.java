80116910cc791cde57c56e3bcad453e5db5b99d5
punithavathis <punithavathisakthivel97@gmail.com>
2017-07-19 15:46:42 +0530

    Add files via upload

---
+++ ppp.java
@@ -0,0 +1,22 @@
+package ss;
+
+import java.util.Scanner;
+
+public class ppp {
+	public static void main(String[] args) {
+		int num=0;
+		int reversenum=0;
+		System.out.println("input the number");
+		Scanner input=new Scanner(System.in);
+		num=input.nextInt();
+while(num!=0){
+	reversenum=reversenum*10;
+	reversenum=reversenum+num%10;
+	num=num/10;}
+	System.out.print(reversenum);
+
+	}
+
+}
+
+
