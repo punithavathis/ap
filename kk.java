71abd4eef319932b676404a7daa16b9977bad22c
punithavathis <punithavathisakthivel97@gmail.com>
2017-07-19 15:08:29 +0530

    Add files via upload

---
+++ kk.java
@@ -0,0 +1,12 @@
+package guviprogram;
+
+public class kk {
+
+	public static void main(String[] args) {
+		String s="200";
+		int i=Integer.parseInt(s);
+		System.out.println(s+100);
+		System.out.println(i+100);
+	}
+
+}
