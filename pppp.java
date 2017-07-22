795dd0bc6c4480f0a005fa359db886b4320ba873
punithavathis <punithavathisakthivel97@gmail.com>
2017-07-22 14:31:51 +0530

    Add files via upload

---
+++ pppp.java
@@ -0,0 +1,18 @@
+package guviprogram;
+import java.util.Scanner;
+public class pppp {
+public static void main(String[] args){
+	int sum=0; 
+	int sc;
+	int n;
+	Scanner in=new Scanner(System.in);
+	System.out.println("enter the number");
+	n=in.nextInt();
+	
+	for(int i=0;i<=n;i++){
+	sum=sum+i;
+	}
+	System.out.println("sum is:"+sum);
+	}
+
+}
