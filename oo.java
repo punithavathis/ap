36419930e80981c60f0dcafca1e8d83c980109d3
punithavathis <punithavathisakthivel97@gmail.com>
2017-07-22 14:15:54 +0530

    Add files via upload

---
+++ oo.java
@@ -0,0 +1,18 @@
+package guviprogram;
+import java.util.Scanner;
+public class oo {
+public static void main(String[] args){
+	int sum=0;
+	int i;
+	for( i=1;i<=15;i++){
+			sum=sum+i;
+}
+	System.out.println(sum);
+	for(i=15;i<=45;i++){
+		if(i%2!=0){
+			System.out.println(i);
+
+		}
+	}
+}
+}
